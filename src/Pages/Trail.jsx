895a5eb9e5eee9b2515963b2513b36f48d6eab11
admaman <aman@ehmad11.com>
2023-06-13 17:17:13 +0500

    api integration

--- src/Pages/Trail.jsx
+++ src/Pages/Trail.jsx
@@ -1,23 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Trail.css'
+import { getTrialBalance } from '../redux/actions/trial.action';
+import { useDispatch,useSelector } from 'react-redux';
+import { Bars } from 'react-loader-spinner';
+
 
 const Trail = () => {
   const [accounts, setAccounts] = useState([]);
-  const [newAccount, setNewAccount] = useState({
-    accountName: '',
-    debitBalance: 0,
-    creditBalance: 0,
+  const dispatch = useDispatch()
+  const [loading,setLoading] = useState(false)
+  const { trial } = useSelector((state) => {
+    return state;
   });
-
-  const handleInputChange = (e) => {
-    setNewAccount({ ...newAccount, [e.target.name]: e.target.value });
-  };
-
-  const handleAddAccount = (e) => {
-    e.preventDefault();
-    setAccounts([...accounts, newAccount]);
-    setNewAccount({ accountName: '', debitBalance: 0, creditBalance: 0 });
-  };
+useEffect(()=>{
+  dispatch(getTrialBalance()).then((res) => {
+    console.log(res)
+    if (res.payload.success) {
+    } else {
+    }
+    setLoading(false)
+  })
+},[])
 
   // Calculate the sum of debit and credit balances
   const totalDebit = accounts.reduce((sum, account) => sum + parseInt(account.debitBalance), 0);
@@ -26,39 +29,8 @@ const Trail = () => {
   return (
     <div className='trail-balance'>
       <h2>Trail Balance</h2>
-      <form onSubmit={handleAddAccount}>
-        <div>
-          <label htmlFor="accountName">Account Name:</label>
-          <input
-            type="text"
-            id="accountName"
-            name="accountName"
-            value={newAccount.accountName}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="debitBalance">Debit Balance:</label>
-          <input
-            type="number"
-            id="debitBalance"
-            name="debitBalance"
-            value={newAccount.debitBalance}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="creditBalance">Credit Balance:</label>
-          <input
-            type="number"
-            id="creditBalance"
-            name="creditBalance"
-            value={newAccount.creditBalance}
-            onChange={handleInputChange}
-          />
-        </div>
-        <button type="submit">Add Account</button>
-      </form>
+      <p>ABC CORPORATION</p>
+      <p>as of 2023</p>
       <table>
         <thead>
           <tr>
@@ -67,20 +39,28 @@ const Trail = () => {
             <th>Credit Balance</th>
           </tr>
         </thead>
-        <tbody>
-          {accounts.map((account, index) => (
-            <tr key={index}>
-              <td>{account.accountName}</td>
-              <td>{account.debitBalance}</td>
-              <td>{account.creditBalance}</td>
+      { !loading ? <tbody>
+          {trial?.payload?.[0]?.tAccounts.map((account, index) => (
+            <tr key={account._id}>
+              <td>{account.account_name}</td>
+              <td>{account.debit}</td>
+              <td>{account.credit}</td>
             </tr>
           ))}
           <tr>
             <td>Total</td>
-            <td>{totalDebit}</td>
-            <td>{totalCredit}</td>
+            <td>{trial?.payload?.[0]?.total?.debit}</td>
+            <td>{trial?.payload?.[0]?.total?.credit}</td>
           </tr>
-        </tbody>
+        </tbody>:
+        <Bars   height="80"
+        width="80"
+        color="#4fa94d"
+        ariaLabel="bars-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true} />
+        }
       </table>
     </div>
   );
